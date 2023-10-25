304a1c611d51b8c6b46e2f3e719720afc4f2a8b5
KakwiriStephen <ngottakakwiri@gmail.com>
2023-10-25 12:41:06 +0300

    Guide Section

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Guide = () => {
   return (
     <section>
-      <div className="border-2 border-red-500 padding-container max-container w-full pb-24">
+      <div className=" padding-container max-container w-full pb-24">
         <Image src="/camp.svg" alt="camp" width={50} height={50} />
 
         <p className="uppercase regular-18 -mt-1 mb-3 text-green-50">
@@ -22,6 +22,33 @@ const Guide = () => {
             and friends to have fun in the wilderness through the valley and
             reach the top level of the mountain.
           </p>
+        </div>
+      </div>
+
+      <div className="flexCenter max-container relative w-full">
+        <Image
+          src="/boat.png"
+          alt="boat"
+          width={1440}
+          height={580}
+          className="w-full object-cover object-center lg:rounded-5xl 2xl:rounded-5xl"
+        />
+
+        <div className="absolute flex bg-white py-8 pl-5 pr-7 gap-3 rounded-3xl border shadow-md md:left-[5%] lg:top-20">
+          <div className="flexBetween flex-col">
+            <div className="flex w-full flex-col">
+              <div className="flexBetween w-full">
+                <p className="regular-16 text-grey-20">Destination</p>
+                <p className="bold-16 text-green-50">48 min</p>
+              </div>
+              <p className="bold-20 mt-2">Anguaas Calientes</p>
+            </div>
+
+            <div className="flex w-full flex-col">
+              <p className="regular-16 text-gay-20"> Start track</p>
+              <h4 className="bold-20 mt-2 whitespace-nowrap">Chasimba Hills</h4>
+            </div>
+          </div>
         </div>
       </div>
     </section>
