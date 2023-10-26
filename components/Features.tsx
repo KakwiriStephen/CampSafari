0bb9e3369402af1b1152fcd9a25155fea17f09ff
KakwiriStephen <ngottakakwiri@gmail.com>
2023-10-26 22:47:24 +0300

    Feature Section

--- components/Features.tsx
+++ components/Features.tsx
@@ -29,13 +29,12 @@ const Features = () => {
             <h2 className="bold-40 lg:bold-64">Our features</h2>
           </div>
 
-          <ul>
+          <ul className="mt-10 grid gap-10 md:grid-cols-2 lg:mg-20 lg:gap-20">
             {FEATURES.map((feature) => (
               <FeatureItem
                 title={feature.title}
                 key={feature.title}
                 icon={feature.icon}
-                variant={feature.variant}
                 description={feature.description}
               />
             ))}
@@ -49,16 +48,21 @@ const Features = () => {
 type FeatureItem = {
   title: string;
   icon: string;
-  variant: string;
   description: string;
 };
 
-const FeatureItem = ({ title, icon, variant, description }: FeatureItem) => {
+const FeatureItem = ({ title, icon, description }: FeatureItem) => {
   return (
     <li className="flex w-full flex-1 flex-col items-start">
       <div className="rounded-full p-4 lg:p-7 bg-green-50">
         <Image src={icon} alt="map" width={28} height={28} />
       </div>
+
+      <h2 className="bold-20 lg:bold-32 mt-5 capitalize">{title}</h2>
+
+      <p className="regular-16 mt-5 bg-white/80 text-gray-30 lg:mt-[30px] lg:bg-none">
+        {description}
+      </p>
     </li>
   );
 };
